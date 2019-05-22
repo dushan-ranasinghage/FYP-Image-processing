1d0dca66660ef848ba6a603451be01ed0a8a252b
MaduRajapaksha <rajapakshamadu6543@gmail.com>
2019-05-22 17:26:25 +0530

    Removing lines and adding contours to the scanned new image

--- preprocessing.py
+++ preprocessing.py
@@ -2,33 +2,37 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-image = cv2.imread('sampleimage.jpg')
-
-plt.subplot(141),plt.imshow(image,  cmap="gray"),plt.title('original')
-plt.xticks([]), plt.yticks([])
-
-kernel = np.ones((5, 5), np.float32) / 25
-dst = cv2.filter2D(image, -1, kernel)
+image = cv2.imread('image1.jpg')
 
 #convert to gray
-gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
-
-#find contours
-ret, thresh = cv2.threshold(gray, 127, 255, 0)
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #binary
-ret,th1 = cv2.threshold(gray,120,255,cv2.THRESH_BINARY)
+ret,th1 = cv2.threshold(gray,180,255,cv2.THRESH_BINARY)
 ret,th2 = cv2.threshold(gray,120,255,cv2.THRESH_BINARY_INV)
 
-plt.subplot(142),plt.imshow(th1,  cmap="gray"),plt.title('binary')
+kernel = np.ones((5,5),np.uint8)
+#erosion = cv2.erode(th1,kernel,iterations = 1)
+
+dilation = cv2.dilate(th1,kernel,iterations = 0)
+
+#find contours
+ret, thresh = cv2.threshold(dilation, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2]
+for contour in contours:
+  cv2.drawContours(dilation, contour, -1, (0, 0, 255), 3)
+plt.figure()
+
+plt.subplot(131),plt.imshow(image),plt.title('original')
 plt.xticks([]), plt.yticks([])
 
-#draw contours
-cv2.drawContours(th1,contours,-1,(0,0,0),3)
-plt.subplot(143),plt.imshow(th1,  cmap="gray"),plt.title('after draw contours')
+plt.subplot(132),plt.imshow(th1,  cmap="gray"),plt.title('binary')
 plt.xticks([]), plt.yticks([])
 
-plt.subplot(144),plt.imshow(th2,  cmap="gray"),plt.title('binary inverse')
+plt.subplot(133),plt.imshow(dilation,  cmap="gray"),plt.title('contours')
 plt.xticks([]), plt.yticks([])
 plt.show()
+
+
