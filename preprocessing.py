05ad013c46c99d06afbd292b2cb02d52ca71cc6e
Dushan Randika <dushanrandika1994@gmail.com>
2019-06-05 12:00:38 +0530

    Sentence bound boxing testing.

--- preprocessing.py
+++ preprocessing.py
@@ -1,39 +1,85 @@
-import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+import numpy
 
-image = cv2.imread('image1.jpg')
+img = cv2.imread('image1.jpg', cv2.COLOR_BGR2GRAY)
+height, width = img.shape[:2]
 
-#convert to gray
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+(_, thresh) = cv2.threshold(gray, 140, 255, cv2.THRESH_BINARY)
+closed = cv2.erode(thresh, None, iterations=7)
+height, width = closed.shape[:2]
 
-#binary
-ret,th1 = cv2.threshold(gray,180,255,cv2.THRESH_BINARY)
-ret,th2 = cv2.threshold(gray,120,255,cv2.THRESH_BINARY_INV)
+z = [0] * height
+v = [0] * width
+hfg = [[0 for col in range(2)] for row in range(height)]
+lfg = [[0 for col in range(2)] for row in range(width)]
+box = [0, 0, 0, 0]
+# Horizontal projection
+a = 0
+emptyImage1 = numpy.zeros((height, width, 3), numpy.uint8)
+for y in range(0, height):
+    for x in range(0, width):
+        cp = closed[y, x]
+        # if np.any(closed[y,x]):
+        if cp == 0:
+            a = a + 1
+        else:
+            continue
+    z[y] = a
+    # print z[y]
+    a = 0
+# Select line split point based on horizontal projection value
+inline = 1
+start = 0
+j = 0
+for i in range(0, height):
+    if inline == 1 and z[i] >= 150:  # Enter the text area from the blank area
+        start = i  # record starting line split point
+        # print i
+        inline = 0
+    elif (i - start > 3) and z[i] < 150 and inline == 0:  # Enter the blank area from the text area
+        inline = 1
+        hfg[j][0] = start - 2  # Save line split position
+        hfg[j][1] = i + 2
+        j = j + 1
 
-kernel = np.ones((5,5),np.uint8)
+# Vertically project and split each line
+a = 0
+for p in range(0, j):
+    for x in range(0, width):
+        for y in range(hfg[p][0], hfg[p][1]):
+            cp1 = closed[y, x]
+            if cp1 == 0:
+                a = a + 1
+            else:
+                continue
+        v[x] = a  # Save each column of pixel values
+        a = 0
+    # print width
+    #
+    incol = 1
+    start1 = 0
+    j1 = 0
+    z1 = hfg[p][0]
+    z2 = hfg[p][1]
+    for i1 in range(0, width):
+        if incol == 1 and v[i1] >= 20:  # Enter the text area from the blank area
+            start1 = i1  # record starting column split point
+            incol = 0
+        elif (i1 - start1 > 3) and v[i1] < 20 and incol == 0:  # Enter the blank area from the text area
+            incol = 1
+            lfg[j1][0] = start1 - 2  # Save column split location
+            lfg[j1][1] = i1 + 2
+            l1 = start1 - 2
+            l2 = i1 + 2
+            j1 = j1 + 1
+            cv2.rectangle(img, (l1, z1), (l2, z2), (255, 0, 0), 2)
 
-dilation = cv2.dilate(th2,kernel,iterations = 0)
+# cv2.namedWindow("main", cv2.WINDOW_NORMAL)
+cv2.imshow('result', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# Calculate horizontal projection
-proj = np.sum(dilation,1)
-
-# Create output image same height as text, 500 px wide
-m = np.max(proj)
-w = 500
-result = np.zeros((proj.shape[0],500))
-
-# Draw a line for each row
-for row in range(dilation.shape[0]):
-   cv2.line(result, (0,row), (int(proj[row]*w/m),row), (255,255,255), 1)
-
-plt.subplot(131),plt.imshow(image),plt.title('original')
-plt.xticks([]), plt.yticks([])
-
-plt.subplot(132),plt.imshow(th1,  cmap="gray"),plt.title('binary')
-plt.xticks([]), plt.yticks([])
-
-plt.subplot(133),plt.imshow(result,  cmap="gray"),plt.title('horizontal projection')
-plt.xticks([]), plt.yticks([])
-plt.show()
-
+#plt.subplot(122),plt.imshow(img , cmap="gray"),plt.title('binary inv')
+#plt.xticks([]), plt.yticks([])
+#plt.show()
